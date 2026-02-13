09adcaf7f8455373c518238a6beb2078f34fd770
farzia <ghaforyfarzia@gmail.com>
2026-02-13 14:08:51 +0100

    Done with commands

--- src/main/java/es/fplumara/dam1/messageKeeper/commands/LastCommand.java
+++ src/main/java/es/fplumara/dam1/messageKeeper/commands/LastCommand.java
@@ -1,4 +1,67 @@
 package es.fplumara.dam1.messageKeeper.commands;
 
-public class LastCommand {
+import es.fplumara.dam1.messageKeeper.exceptions.CommandException;
+import es.fplumara.dam1.messageKeeper.services.files.DefaultFileService;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+import java.util.List;
+
+public class LastCommand implements BotCommand {
+
+    private final DefaultFileService fileService;
+
+    public LastCommand(DefaultFileService fileService) {
+        this.fileService = fileService;
+    }
+
+    @Override
+    public String name() {
+        return "!last";
+    }
+
+    @Override
+    public void execute(MessageReceivedEvent event, String[] args)
+            throws CommandException {
+
+        if (args.length < 2) {
+            throw new CommandException("Usage:!last n");
+        }
+
+        int n;
+
+        try {
+            n = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            throw new CommandException("n must be a number");
+        }
+
+        String channelId = event.getChannel().getId();
+
+        try {
+            List<String> lines =
+                    fileService.readLast(channelId, n);
+
+            if (lines.isEmpty()) {
+                event.getChannel()
+                        .sendMessage("No history")
+                        .queue();
+                return;
+            }
+
+            String response = String.join("\n", lines);
+
+            if (response.length() > 1900) {
+                response = response.substring(0, 1900)
+                        + ("truncated");
+            }
+
+            event.getChannel()
+                    .sendMessage(" " + response + " ")
+                    .queue();
+
+        } catch (Exception e) {
+            throw new CommandException("Error reading history",e);
+        }
+    }
 }
+
--- src/main/java/es/fplumara/dam1/messageKeeper/commands/LogOnCommand.java
+++ src/main/java/es/fplumara/dam1/messageKeeper/commands/LogOnCommand.java
@@ -1,4 +1,33 @@
 package es.fplumara.dam1.messageKeeper.commands;
 
-public class LogOnCommand {
+import es.fplumara.dam1.messageKeeper.config.AppConfig;
+import es.fplumara.dam1.messageKeeper.exceptions.CommandException;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public class LogOnCommand implements BotCommand {
+
+    private final AppConfig config;
+
+    public LogOnCommand(AppConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public String name() {
+        return "!log";
+    }
+
+    @Override
+    public void execute(MessageReceivedEvent event, String[] args)
+            throws CommandException {
+
+        if (args.length < 2 || !args[1].equalsIgnoreCase("on")) {
+            return;
+        }
+
+        config.setLogsEnabled(true);
+        event.getChannel()
+                .sendMessage("Logging enabled")
+                .queue();
+    }
 }
